22a1da650ef918d9715ca70bb36dbd7ba36c92ab
Pip3r4o <pip3r4o@gmail.com>
2016-01-22 10:24:49 +0200

    Web Forms Project Scaffold

---
+++ LINQdIn/LINQdIn/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LINQdIn.Startup))]
+namespace LINQdIn
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
