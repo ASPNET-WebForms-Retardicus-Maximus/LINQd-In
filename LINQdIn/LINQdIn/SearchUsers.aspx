e0bd432d0550d3076d5be1fe038dd7afb8d88ce4
KonstantinSimeonov <kon.simeonov@gmail.com>
2016-01-24 15:17:58 +0200

    Users page
    such paging, much glory

---
+++ LINQdIn/LINQdIn/SearchUsers.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SearchUsers.aspx.cs" Inherits="LINQdIn.SearchUsers" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="GridViewUsers" runat="server" AutoGenerateColumns="False" CssClass="table table-striped table-responsive table-hover"
+        AllowPaging="True" DataKeyNames="ID"
+        OnPageIndexChanging="GridViewUsers_PageIndexChanging"
+        OnSelectedIndexChanged="GridViewUsers_SelectedIndexChanged"
+        OnRowDataBound="GridViewUsers_RowDataBound">
+        <Columns>
+            <asp:BoundField DataField="Name" HeaderText="Name" />
+            <asp:BoundField DataField="Skills" HeaderText="Skills" />
+            <asp:HyperLinkField Text="Details" DataNavigateUrlFields="Id" ControlStyle-CssClass="btn btn-sm btn-raised btn-primary"
+                DataNavigateUrlFormatString="PlaceholderReplaceWithUserDetailsPageLazySlave.aspx?id={0}" />
+        </Columns>
+    </asp:GridView>
+    <asp:Label ID="LabelSelectedItem" runat="server"></asp:Label>
+</asp:Content>
---
+++ LINQdIn/LINQdIn/SearchUsers.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace LINQdIn
+{
+    using Ninject;
+    using Services;
+
+    public partial class SearchUsers : System.Web.UI.Page
+    {
+        [Inject]
+        public IUserService UserService { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            this.GridViewUsers.DataSource = UserService.GetAllNonAdmin()
+                                                .ToList().Select(x => new
+                                                {
+                                                    ID= x.Id,
+                                                    Name = x.FirstName + " " + x.LastName,
+                                                    Skills = string.Join(", ", x.Skills.Select(y => y.Name))
+                                                }).ToList();
+            this.GridViewUsers.DataBind();
+        }
+
+        protected void GridViewUsers_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+
+        protected void GridViewUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridViewUsers_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+    }
+}
